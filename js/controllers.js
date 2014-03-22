c356069b03b1d475a05ab3eeaca8e0bd3e7dc3a4
wccah <william.c.cahill@gmail.com>
2014-03-22 16:40:42 -0400

    .

--- js/controllers.js
+++ js/controllers.js
@@ -6,9 +6,12 @@ checkerApp.controller('CheckerCtrl', function($scope) {
 	var grid = [];
 	var a = true;
 	for(var i=0;i<8;i++){
+		var c = [];
 		for(var j=0;j<8;j++){
 			a = !a;
-		}	
+			c.push({color:a});
+		}
+		grid.push(c);
 	}
 	
 	$scope.grid = grid;
