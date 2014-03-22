ac95ad4f247e2264b1d4fcbe2bc86d7da845e42a
wccah <william.c.cahill@gmail.com>
2014-03-22 17:23:16 -0400

    board setup

--- js/controllers.js
+++ js/controllers.js
@@ -7,13 +7,29 @@ checkerApp.controller('CheckerCtrl', function($scope) {
 	var a = true;
 	for(var i=0;i<8;i++){
 		var c = [];
+		var p = i<3 ? -1 :
+			     (i>4 ? 1 : 0);
+
 		for(var j=0;j<8;j++){
 			a = !a;
-			c.push({color:a});
+			c.push({on:a, x:j, y:i, piece: a?p:0});
 		}
 		a = !a;
 		grid.push(c);
 	}
 	
 	$scope.grid = grid;
+	$scope.clicked = { x: undefined, y: undefined };
+	
+	
+	
+	$scope.clickSquare = function(cell) {
+		debugger;
+		if(!cell.on) {
+			return;
+		}
+		
+		$scope.clicked.x = cell.x;
+		$scope.clicked.y = cell.y;
+	};
 });
